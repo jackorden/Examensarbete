ada0106175ee9fa7012643522a73d45f394528c6
jack <jack@tango.jack>
2024-02-25 21:53:04 +0100

    Har autentiseringsuppgifter synliga, fixare senare.

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,36 +1,21 @@
 terraform {
 
-    required_version = ">= 0.13.0"
+    required_version = ">= 0.14"
 
     required_providers {
         proxmox = {
-            source = "telmate/proxmox"
-            version = "3.0.1-rc1"
+            source = "bpg/proxmox"
+            version = ">= 0.46.6"
         }
 
     }
 
 }
 
-variable "proxmox_api_url" {
-    type = string
-}
-
-variable "proxmox_api_token_id" {
-    type = string
-    sensitive = true
-}
-
-variable "proxmox_api_token_secret" {
-    type = string
-    sensitive = true
-}
-
 provider "proxmox" {
 
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_scret = var.proxmox_api_token_secret
-
-    pm_tils_insecure = true
+    endpoint = "https://10.6.67.221:8006"
+    api_token = "root@pam!terraform=f337998e-a057-44f1-bf6f-32bcd68031e9"
+# Kräver ej https
+    insecure = true
 }
