c3693b8e2e3946a105114dfb46ec5c5913581179
jack <jack@tango.jack>
2024-02-25 21:52:22 +0100

    Variables used in the ubuntu-test.tf file

---
+++ Terraform/vars.tf
@@ -0,0 +1,36 @@
+variable "vm_config" {
+    type = list(object({
+        idx = number
+        name = string
+        vmid_to_clone = number
+        cpu = number
+        ram = number
+        size = number
+    }))
+    default = [
+    {
+        idx = 0
+        name = "tango-test1"
+        vmid_to_clone = 1000
+        cpu = 1
+        ram = 2048
+        size = 32
+    },
+    {
+        idx = 1
+        name = "tango-test2"
+        vmid_to_clone = 1000
+        cpu = 1
+        ram = 2048
+        size = 32
+    },
+    {
+        idx = 2
+        name = "tango-test3"
+        vmid_to_clone = 1000
+        cpu = 1
+        ram = 2048
+        size = 32
+    }
+    ]
+}
