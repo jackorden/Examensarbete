dff751dde157fe8f27f664d8b13a5d23e3f8f910
jack <jackbogneorden@gmail.com>
2024-03-06 15:05:20 +0100

    testing.

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
 
     required_version = ">= 0.14"
 
-    required_providers {
+    reqsded_providers {
         proxmox = {
             source = "bpg/proxmox"
             version = ">= 0.46.6"
