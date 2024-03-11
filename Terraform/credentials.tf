015d2dde4754a0d07de337d823ab5de04419e037
jack <jack@tango.jack>
2024-03-11 11:25:47 +0100

    test

--- Terraform/credentials.tf
+++ Terraform/credentials.tf
@@ -1,9 +1,7 @@
 variable "virtual_environment_endpoint" {
-    type = string
     default = "https://10.6.67.221:8006"
 }
 
 variable "virtual_environment_password" {
-    type = string
     default = "root@pam!terraform=f337998e-a057-44f1-bf6f-32bcd68031e9"
 }
